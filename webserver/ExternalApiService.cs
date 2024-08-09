6601925fc8309373ea12623fd46f688665cdea9b
gcz3zgv <alexanderpalacios@ups.com>
2024-08-09 16:43:54 -0400

    Added dummy return value for webserver

--- webserver/ExternalApiService.cs
+++ webserver/ExternalApiService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using webserver.Models;
 using Microsoft.AspNetCore.WebUtilities;
@@ -18,10 +19,40 @@ public class ExternalApiService
 
     public async Task<string> GetFieldsAsync(Dictionary<string, string> queryParams)
     {
-        var url = QueryHelpers.AddQueryString($"{BaseUrl}/fields", queryParams);
-        var response = await _httpClient.GetAsync(url);
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsStringAsync();
+        // var url = QueryHelpers.AddQueryString($"{BaseUrl}/fields", queryParams);
+        // var response = await _httpClient.GetAsync(url);
+        // response.EnsureSuccessStatusCode();
+        // return await response.Content.ReadAsStringAsync();
+        List<Field> fields = new List<Field>
+        {
+            new Field
+            {
+                alias = 1,
+                appName = "App1",
+                validationRule = new ValidationRule
+                {
+                    Rule_Id = 101,
+                    Name = "Rule1",
+                    Description = "Description1",
+                    Regex = "Regex1"
+                }
+            },
+            new Field
+            {
+                alias = 2,
+                appName = "App2",
+                validationRule = new ValidationRule
+                {
+                    Rule_Id = 102,
+                    Name = "Rule2",
+                    Description = "Description2",
+                    Regex = "Regex2"
+                }
+            }
+        };
+
+        var json = JsonConvert.SerializeObject(fields);
+        return json;
     }
 
     public async Task<string> PostFieldsAsync(object data)
